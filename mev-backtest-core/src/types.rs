7b2bb2c54b69a7ac399a13869ff3e0b80c1b37f0
moha-moradi <moradi.mohammad028@gmail.com>
2026-06-07 19:48:03 +0330

    feat: add serde derives to RangeMode

--- mev-backtest-core/src/types.rs
+++ mev-backtest-core/src/types.rs
@@ -202,7 +202,7 @@ impl Strategy {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub enum RangeMode {
     Days(u64),
     Blocks(u64),
